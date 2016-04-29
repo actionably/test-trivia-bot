6845e99b1b9ca2095d5b350e8893b0b2eea5f164
Jesse Hull <jessehull@gmail.com>
2016-04-28 17:36:15 -0700

    send request type and send outgoing messages

--- src/actionably.js
+++ src/actionably.js
@@ -10,7 +10,7 @@ class Actionably {
     console.log('Incoming');
     console.log(JSON.stringify(data, null, 2));
     rp({
-      uri: `http://localhost:3000/track?apiKey=${this.apiKey}`,
+      uri: `http://localhost:3000/track?apiKey=${this.apiKey}&type=incoming`,
       method: 'POST',
       json: data
     });
@@ -18,11 +18,24 @@ class Actionably {
   logOutgoing(data) {
     console.log('Outgoing');
     console.log(JSON.stringify(data, null, 2));
+    rp({
+      uri: `http://localhost:3000/track?apiKey=${this.apiKey}&type=outgoing`,
+      method: 'POST',
+      json: data
+    });
   }
   logOutgoingResponse(error, response) {
     console.log('Outgoing response');
     console.log(JSON.stringify(error, null, 2));
     console.log(JSON.stringify(response.body, null, 2));
+    rp({
+      uri: `http://localhost:3000/track?apiKey=${this.apiKey}&type=outgoingResponse`,
+      method: 'POST',
+      json: {
+        error: error,
+        responseBody: response.body
+      }
+    });
   }
 }
 
